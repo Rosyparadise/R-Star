398c9a471b14e7ebad3a885bd5efb62845a22a60
Rosyparadise <cevangel@csd.auth.gr>
2022-05-10 21:47:57 +0300

    node and block seperation, write data to first block

--- RStarTree/src/RStarTree.java
+++ RStarTree/src/RStarTree.java
@@ -9,11 +9,18 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class RStarTree
 {
+    private static long noOfNodes;
+    private static long noOfBlocks;
+    private static char delimiter = '$';
+    private static char blockSeperator = '#';
+
 
 
     public static byte[] longToBytes(long x) {
@@ -27,6 +34,14 @@ public class RStarTree
         buffer.putDouble(x);
         return buffer.array();
     }
+
+    public static byte[] charToBytes(Character x) {
+        ByteBuffer buffer = ByteBuffer.allocate(Character.BYTES);
+        buffer.putChar(x);
+        return buffer.array();
+    }
+
+
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException
     {
@@ -59,15 +74,19 @@ public class RStarTree
             //node block represents the <node> we are currently processing
             Node block;
             //noOfNodes is the number of <nodes> in the .osm file
-            int noOfNodes = doc.getElementsByTagName("node").getLength();
+            noOfNodes = doc.getElementsByTagName("node").getLength();
+
+
             //data to save
+            int bytecounter=0;
             long id;
             String name=null;
             double lat, lon;
 
             //where the data will be saved after being converted into byte arrays
             byte[] b_id, b_lat, b_lon, b_name=null;
-
+            //first block will overwrite this with data
+            dataWriter.write("xxxxxxxxxxxxxxxx".getBytes());
             //for every <node>
             for (int i = 0; i < noOfNodes; i++)
             {
@@ -98,7 +117,7 @@ public class RStarTree
                         {
                             //if there is, save the value of attribute v as the name
                             name = children.item(j).getAttributes().getNamedItem("v").getNodeValue();
-                            b_name = children.item(j).getAttributes().getNamedItem("v").getNodeValue().getBytes();
+                            b_name = children.item(j).getAttributes().getNamedItem("v").getNodeValue().getBytes(StandardCharsets.UTF_8);
                             break;
                         }
                     }
@@ -108,16 +127,22 @@ public class RStarTree
                     dataWriter.write(b_id);dataWriter.write(b_lat);dataWriter.write(b_lon);
                     System.out.println(id + " "+ lat +" "+lon);
                     //dataWriter.write(id + " " + lat + " " + lon);
-                    //bytecounter+=24;
+                    bytecounter+=26;
                 }
                 else {
                     //dataWriter.write(id + " " + lat + " " + lon + " " + name);
                     dataWriter.write(b_id);dataWriter.write(b_lat);dataWriter.write(b_lon);dataWriter.write(b_name);
-                    System.out.println(id + " "+ lat +" "+lon);
+                    System.out.println(id + " "+ lat +" "+lon + " " + name);
                     //dataWriter.write(id + " " + lat + " " + lon + " " + name);
-                    //bytecounter+=(24+b_name.length);
+                    bytecounter+=(26+b_name.length);
                 }
-                dataWriter.write("\n".getBytes());
+                dataWriter.write(charToBytes(delimiter));
+                if (bytecounter+26>32000)
+                {
+                    dataWriter.write(charToBytes(blockSeperator));
+                    noOfBlocks++;
+                    bytecounter=0;
+                }
 
                 name=null;
                 b_name=null;
@@ -127,6 +152,12 @@ public class RStarTree
             dataWriter.close();
 
 
+            //first block
+            RandomAccessFile datafile = new RandomAccessFile("datafile.txt", "rw");
+            datafile.seek(0);
+            datafile.write(longToBytes(noOfBlocks));
+            datafile.write(longToBytes(noOfNodes));
+            datafile.close();
         }
 
         catch (IOException e)
@@ -135,32 +166,106 @@ public class RStarTree
             e.printStackTrace();
         }
 
+
         try {
             File file = new File("datafile.txt");
+            //byte arrays to save the byte representation of the variables
             byte[] bytes = Files.readAllBytes(file.toPath());
-
+            byte[] nodesArray = new byte[8],BlocksArray = new byte[8];
+            byte[] delimiterArray = new byte[2],blockSeperatorArray = new byte[2];
             byte[] NodeIdArray = new byte[8];
-            byte[] LatArray = new byte[8];
-            byte[] LonArray = new byte[8];
+            byte[] LatArray = new byte[8],LonArray = new byte[8];
+            //current byte
+            int bytecounter=0;
+            //actual variables
+            long tempNodeId;
+            double tempLat,tempLon;
+            String tempName="";
+            char newlinestr;
 
-            System.arraycopy(bytes, 0, NodeIdArray, 0, 8);
-            System.arraycopy(bytes, 8, LatArray, 0, 8);
-            System.arraycopy(bytes, 16, LonArray, 0, 8);
+            //reading first block
+            System.arraycopy(bytes, 0, BlocksArray, 0, 8);
+            System.arraycopy(bytes, 8, nodesArray, 0, 8);
+            System.out.println("Number of blocks= " + ByteBuffer.wrap(BlocksArray).getLong());
+            System.out.println("Number of nodes= " + ByteBuffer.wrap(nodesArray).getLong());
+            bytecounter+=16;
 
-            long tempNodeId = ByteBuffer.wrap(NodeIdArray).getLong();
-            double tempLat = ByteBuffer.wrap(LatArray).getDouble();
-            double tempLon = ByteBuffer.wrap(LonArray).getDouble();
 
-            System.out.println("Node id: " + tempNodeId);
-            System.out.println("LAT: " + tempLat);
-            System.out.println("LON: " + tempLon);
+
+            for (int i=0;i<noOfNodes;i++)
+            {
+
+                //byte[] nameArray = new byte[18];
+                System.arraycopy(bytes, bytecounter, NodeIdArray, 0, 8);
+                System.arraycopy(bytes, bytecounter+8, LatArray, 0, 8);
+                System.arraycopy(bytes, bytecounter+16, LonArray, 0, 8);
+
+                System.arraycopy(bytes, bytecounter+24, delimiterArray,0,2);
+
+                tempNodeId = ByteBuffer.wrap(NodeIdArray).getLong();
+                tempLat = ByteBuffer.wrap(LatArray).getDouble();
+                tempLon = ByteBuffer.wrap(LonArray).getDouble();
+                newlinestr = ByteBuffer.wrap(delimiterArray).getChar();
+
+                int tempcounter=0;
+
+                //count bytes until you meet the delimiter if not already met above
+                while (newlinestr!=delimiter)
+                {
+                    tempcounter+=1;
+                    System.arraycopy(bytes, bytecounter+24+tempcounter, delimiterArray,0,2);
+                    newlinestr = ByteBuffer.wrap(delimiterArray).getChar();
+
+
+                }
+
+                //write name
+                if (tempcounter!=0)
+                {
+                    byte[] nameArray = new byte[tempcounter];
+                    System.arraycopy(bytes, bytecounter+24, nameArray,0,tempcounter);
+                    tempName = new String(nameArray);
+
+                }
+
+
+                System.out.println("Node id: " + tempNodeId);
+                System.out.println("LAT: " + tempLat);
+                System.out.println("LON: " + tempLon);
+                System.out.println("nl: "+ newlinestr);
+
+
+                if (tempcounter!=0)
+                {
+                    System.out.println("Name: "+ tempName);
+                    bytecounter+=26+tempcounter;
+
+                }
+                else
+                    bytecounter+=26;
+
+
+
+                if (i+1<noOfNodes) {
+                    System.arraycopy(bytes, bytecounter, blockSeperatorArray, 0, 2); // DOESNT ADD BYTES IF FILE ENDS WITH #
+                    if (ByteBuffer.wrap(blockSeperatorArray).getChar()==blockSeperator)
+                        bytecounter+=2;
+                }
+                /*
+                else
+                {
+                    if (bytes.length>bytecounter)
+                        bytecounter+=2;
+                }
+                tempcounter=0;
+                */
+            }
+            System.out.print(bytecounter);
+
 
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-
     }
-
 }
-
