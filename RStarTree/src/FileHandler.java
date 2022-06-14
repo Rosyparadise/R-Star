f76b7e7a89bf2f580bf4df787949c71e69dcf48b
Thanos Nakas <athanani@csd.auth.gr>
2022-06-14 16:08:09 +0300

    delete function

--- RStarTree/src/FileHandler.java
+++ RStarTree/src/FileHandler.java
@@ -5,19 +5,13 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.awt.*;
-import java.awt.image.AreaAveragingScaleFilter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.lang.reflect.Array;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.security.spec.KeySpec;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class FileHandler {
     private static int root=-1;
@@ -1003,14 +997,12 @@ public class FileHandler {
             int tempParentPointer = ByteBuffer.wrap(parentPointer).getInt();
 
             // first if bracket in CS2
-
             if (tempLevel == leafLevel)
             {
                 return currentBlock;
             }
             else if (tempLevel + 1 == leafLevel)
             {
-
                 ArrayList<double[][]> rectangles = new ArrayList<>();
                 double[][] temp = new double[dimensions][dimensions];
                 int[] IDs = new int[tempCurrentNoOfEntries];
@@ -1037,21 +1029,11 @@ public class FileHandler {
                     temp = new double[dimensions][dimensions];
                 }
 
-
                 int result = determine_best_insertion(rectangles, record);
                 return ChooseSubtree(record, IDs[result]);
 
-
-
-
-
-
                 // change currentblock so that the function works recursively
-
-
             }
-
-
             else
             {
                 return 1;
@@ -1222,5 +1204,114 @@ public class FileHandler {
         // minus the left childpointer of the first rectangle (the first rectangle is the only one with two childpointers)
         // and all mod with rectangleInfoSize
         return (blockSize - metadataSize) / nodeInfoSize;
+    }
+
+    public static void delete(double LAT, double LON)
+    {
+        boolean result = delete(LAT, LON, 1);
+        if (result)
+        {
+            System.out.println("The node with LAT: " + LAT + ", and LON: " + LON + ", was successfully deleted.");
+        }
+        else
+        {
+            System.out.println("The node with the given coordinates didn't get found.");
+        }
+    }
+
+    private static boolean delete(double LAT, double LON, int block)
+    {
+        try
+        {
+            File file = new File(IndexfilePath);
+            byte[] bytes = Files.readAllBytes(file.toPath());
+            byte[] dataBlock = new byte[blockSize];
+            System.arraycopy(bytes, block * blockSize, dataBlock, 0, blockSize);
+
+            byte[] blockLevel = new byte[Integer.BYTES];
+            byte[] noOfEntries = new byte[Integer.BYTES];
+
+            System.arraycopy(dataBlock, 0, blockLevel, 0, Integer.BYTES);
+            System.arraycopy(dataBlock, Integer.BYTES, noOfEntries, 0, Integer.BYTES);
+
+            int tempBlockLevel = ByteBuffer.wrap(blockLevel).getInt();
+            int tempNoOfEntries = ByteBuffer.wrap(noOfEntries).getInt();
+
+            // NoOfEntries + block level + parent pointer
+            int bytecounter = 3 * Integer.BYTES;
+
+            if (tempBlockLevel == leafLevel)
+            {
+                byte[] LatArray = new byte[Double.BYTES];
+                byte[] LonArray = new byte[Double.BYTES];
+
+                for (int i = 0; i < tempNoOfEntries; i++)
+                {
+                    System.arraycopy(dataBlock, bytecounter, LatArray, 0, Double.BYTES);
+                    System.arraycopy(dataBlock, bytecounter + Double.BYTES, LonArray, 0, Double.BYTES);
+
+                    if (LAT == ByteBuffer.wrap(LatArray).getDouble() && LON == ByteBuffer.wrap(LonArray).getDouble())
+                    {
+                        int tempBytecounter = bytecounter + (tempNoOfEntries - i - 1) * (2 * Double.BYTES + Integer.BYTES);
+
+                        // copy the data from the last entry into the space of the entry that gets deleted
+                        System.arraycopy(dataBlock, tempBytecounter, dataBlock, bytecounter, Double.BYTES);
+                        System.arraycopy(dataBlock, tempBytecounter + Double.BYTES, dataBlock, bytecounter + Double.BYTES, Double.BYTES);
+                        System.arraycopy(dataBlock, tempBytecounter + 2 * Double.BYTES, dataBlock, bytecounter + 2 * Double.BYTES, Integer.BYTES);
+
+                        // empty the data from the entry copied
+                        System.arraycopy(new byte[Double.BYTES], 0, dataBlock, tempBytecounter, Double.BYTES);
+                        System.arraycopy(new byte[Double.BYTES], 0, dataBlock, tempBytecounter + Double.BYTES, Double.BYTES);
+                        System.arraycopy(new byte[Integer.BYTES], 0, dataBlock, tempBytecounter + 2 * Double.BYTES, Integer.BYTES);
+
+                        // decrease the noOfEntries
+                        tempNoOfEntries--;
+                        System.arraycopy(intToBytes(tempNoOfEntries), 0, dataBlock, Integer.BYTES, Integer.BYTES);
+
+                        RandomAccessFile indexfile = new RandomAccessFile(IndexfilePath, "rw");
+
+                        indexfile.seek((long) blockSize * block);
+                        indexfile.write(dataBlock);
+                        indexfile.close();
+
+                        return true;
+                    }
+
+                    // LAT + LON + recordId
+                    bytecounter += 2 * Double.BYTES + Integer.BYTES;
+                }
+                return false;
+            }
+            else
+            {
+                byte[] firstLatArray = new byte[Double.BYTES];
+                byte[] firstLonArray = new byte[Double.BYTES];
+                byte[] secondLatArray = new byte[Double.BYTES];
+                byte[] secondLonArray = new byte[Double.BYTES];
+                byte[] childPointer = new byte[Double.BYTES];
+
+                for (int i = 0; i < tempNoOfEntries; i++)
+                {
+                    System.arraycopy(dataBlock, bytecounter, firstLatArray, 0, Double.BYTES);
+                    System.arraycopy(dataBlock, bytecounter + Double.BYTES, firstLonArray, 0, Double.BYTES);
+                    System.arraycopy(dataBlock, bytecounter + 2 * Double.BYTES, secondLatArray, 0, Double.BYTES);
+                    System.arraycopy(dataBlock, bytecounter + 3 * Double.BYTES, secondLonArray, 0, Double.BYTES);
+                    System.arraycopy(dataBlock, bytecounter + 4 * Double.BYTES, childPointer, 0, Integer.BYTES);
+
+                    bytecounter += 4 * Double.BYTES + Integer.BYTES;
+
+                    if (ByteBuffer.wrap(firstLatArray).getDouble() <= LAT && LAT <= ByteBuffer.wrap(secondLatArray).getDouble()
+                        && ByteBuffer.wrap(firstLonArray).getDouble() <= LON && LON <= ByteBuffer.wrap(secondLonArray).getDouble())
+                    {
+                        return delete(LAT, LON, ByteBuffer.wrap(childPointer).getInt());
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
--- RStarTree/src/Main.java
+++ RStarTree/src/Main.java
@@ -19,5 +19,15 @@ public class Main {
 
         // read indexfile metadata (first block)
 //        FileHandler.readFirstIndexfileBlock();
+
+        // delete
+        double LAT = 41.3850909;
+        double LON = 26.6241053;
+        FileHandler.delete(LAT, LON);
+
+        FileHandler.readIndexFile();
     }
 }
+
+// LAT: 41.3850909, LON: 26.6241053, ID:349, Datafile block: 1, Block slot: 9113 node to be deleted in the first block
+// LAT: 41.4930671, LON: 26.5358795, ID:786, Datafile block: 1, Block slot: 20482 node to replace the deleted node ^
